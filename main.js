77e1433e0c73891343e3bbee19cf21d93d9883fb
Kavinvit <k7.n3wton@gmail.com>
2026-02-02 17:26:14 +0700

    finish dynamic-temperature feature

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-  const weatherData = await.weatherPromise.json()
-  console.log(weatherData)
+  const weatherData = await weatherPromise.json()
+
+  const ourTemperature = weatherData.properties.periods[0].temperature * 1.8 + 32
+  document.querySelector("#temperature-output").textContent = ourTemperature
 }
 
 start()
