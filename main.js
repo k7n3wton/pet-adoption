27aa5b95d9d9c51eb06ee3f6122571c82c406347
Kavinvit <k7.n3wton@gmail.com>
2026-02-02 15:43:15 +0700

    add main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await.weatherPromise.json()
+  console.log(weatherData)
+}
+
+start()
